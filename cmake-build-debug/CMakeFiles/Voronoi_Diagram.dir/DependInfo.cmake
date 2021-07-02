9525c845e05ba605b53c5ee6f23c9359a9b9d42c
CourteousSleet <me756478@gmail.com>
2021-07-03 02:23:15 +0300

    Begginng of creating that shit

---
+++ cmake-build-debug/CMakeFiles/Voronoi_Diagram.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Xiaomi/Documents/Programming/C++/Projects/Voronoi_Diagram/main.cpp" "C:/Users/Xiaomi/Documents/Programming/C++/Projects/Voronoi_Diagram/cmake-build-debug/CMakeFiles/Voronoi_Diagram.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../headers"
+  "/usr/include"
+  "c:/SFML/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
