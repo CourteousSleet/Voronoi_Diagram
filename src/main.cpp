0757114d69d314434118a4f5760a9e0bfbc831b0
CourteousSleet <me756478@gmail.com>
2021-07-03 20:13:55 +0300

    Added missing conditions for Win32 platform. Now this programme even starts

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
+#include <filesystem>
 #include <SFML/Graphics.hpp>
 
 int main() {
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Voronoi Diagram");
 
-  return 0;
+  // Load a sprite to display
+  sf::Texture texture;
+  if (!texture.loadFromFile(R"(..\images\visualization.png)"))
+    return EXIT_FAILURE;
+  sf::Sprite sprite(texture);
+
+  // Start the game loop
+  while (window.isOpen()) {
+    // Process events
+    sf::Event event{};
+    while (window.pollEvent(event)) {
+      // Close window: exit
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    // Clear screen
+    window.clear();
+    // Draw the sprite
+    window.draw(sprite);
+    // Update the window
+    window.display();
+  }
+  return EXIT_SUCCESS;
 }
