9fd5527b4f6aebcb9f5ec0120e679de8044ca104
CourteousSleet <me756478@gmail.com>
2021-07-07 01:47:27 +0300

    Added stupid icon and drawing line (these changes don't deserve committing nevertheless I commit them)

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,39 @@
 
 double const eps = 0.2;
 
-struct less
-{
-  bool operator () (double l, double r) const { return l < r; }
+struct less {
+  bool operator()(double l, double r) const { return l < r; }
   using is_transparent = void;
-  bool operator () (int l, double r) const { return l + eps < r; }
-  bool operator () (double l, int r) const { return l + eps < r; }
+  bool operator()(int l, double r) const { return l + eps < r; }
+  bool operator()(double l, int r) const { return l + eps < r; }
 };
 
 int main() {
   // Create the main window
   sf::RenderWindow window(sf::VideoMode(800, 600), "Voronoi Diagram");
 
+  auto icon = sf::Image();
+#ifdef unix
+  if (!icon.loadFromFile("(../images/icon.png)"))
+#else
+  if (!icon.loadFromFile(R"(..\images\icon.png)"))
+#endif //unix
+    return EXIT_FAILURE;
+  window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+
   // Limit the framerate to 60 frames per second (this step is optional)
   window.setFramerateLimit(120);
 
+  window.setVerticalSyncEnabled(true);
+
+  sf::RectangleShape sweep_line(sf::Vector2f(150.f, 5.f));
+  sweep_line.rotate(90.f);
+  sweep_line.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+  sweep_line.setFillColor(sf::Color::Red);
+  sweep_line.setOutlineThickness(2);
+
+  /*
   // Load a sprite to display
   sf::Texture texture;
 #ifdef unix
@@ -30,7 +48,7 @@ int main() {
 #endif //unix
     return EXIT_FAILURE;
   sf::Sprite sprite(texture);
-
+  */
 
 
 
@@ -47,14 +65,15 @@ int main() {
     // Activate the window for OpenGL rendering
     window.setActive();
 
-    /* Here is the main code of drawing with OpenGL
-     *
-     */
+    // Here is the main code of drawing
 
     // Clear screen
-    window.clear();
+    window.clear(sf::Color(255, 255, 255, 0));
+    window.draw(sweep_line);
+
     // Draw the sprite
-    window.draw(sprite);
+    //window.draw(sprite);
+
     // Update the window
     window.display();
   }
